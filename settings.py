47e770775579a061c3149b2020d55ce36ec98759
metravod <arttrek42@gmail.com>
2023-08-09 16:07:01 +0300

    baseline 2.0

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
-ws_uri = 'wss://test-ws.skns.dev/raw-messages'
+ws_uri_receive = 'wss://test-ws.skns.dev/raw-messages'
+ws_uri_send = 'wss://test-ws.skns.dev/ordered-messages/lyadov'
 chunk_size = 1000
--- ws_client.py
+++ ws_client.py
@@ -1,30 +1,47 @@
 import json
-import time
-import logging
+from typing import Any
 from websocket import create_connection
 
-from settings import ws_uri, chunk_size
-
-logger = logging.getLogger('ws_client')
-
-
-# Подключение к вебсокету
-ws = create_connection(ws_uri)
+from settings import ws_uri_receive, ws_uri_send, chunk_size
+from timeit import timeit
 
 
-def key_for_sort(dictionary: dict) -> int:
-    return dictionary['id']
+class WsOrderedMessages:
+
+    def __init__(self, uri_receive: str, uri_send: str, chunk_size: int):
+        self.uri_receive = uri_receive
+        self.uri_send = uri_send
+        self.chunk_size = chunk_size
+        self.key_for_sort = 'id'
+
+    def run(self) -> None:
+        self._ws_receive = self._create_conn(self.uri_receive)
+        self._ws_release = self._create_conn(self.uri_send)
+        while True:
+            self._get_chunk()
+            self._sort_chunk()
+            self._send_chunk()
+
+    @staticmethod
+    def _create_conn(uri: str) -> Any:
+        # Подключение к вебсокету
+        return create_connection(uri)
+
+    @timeit
+    def _get_chunk(self) -> None:
+        self._chunk = []
+        while len(self._chunk) < self.chunk_size:
+            res = json.loads(self._ws_receive.recv())
+            self._chunk.append(res)
+
+    @timeit
+    def _sort_chunk(self) -> None:
+        self._chunk.sort(key=lambda dictionary: dictionary[self.key_for_sort])
+
+    @timeit
+    def _send_chunk(self) -> None:
+        self._ws_release.send(json.dumps(self._chunk))
 
 
-# В бесконечном цикле наполняем список до 100 элементов
-# потом сортируем его
-while True:
-    chunk_recv = []
-    ts = time.time()
-    while len(chunk_recv) < chunk_size:
-        result = json.loads(ws.recv())
-        chunk_recv.append(result)
-    tc = time.time()
-    chunk_recv.sort(key=key_for_sort)
-    te = time.time()
-    logger.warning(f'new chunk - forming {tc - ts} - sort {te - tc} -> {len(chunk_recv)}')
+if __name__ == '__main__':
+    WsOrderedMessages(ws_uri_receive, ws_uri_send, chunk_size).run()
